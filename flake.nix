72f37dee5c956c2a99bd0d8e0c76e28f79fc203f
HuangYi <huang@crypto.com>
2022-04-27 21:13:07 +0800

    Problem: no relocatable rocksdb binary in CI
    Closes: #444
    Solution:
    - build relocatable rocksdb binary using nix

--- flake.nix
+++ flake.nix
@@ -1,33 +1,44 @@
 {
   inputs = {
-    nixpkgs = {
-      url = "path:./nix";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-21.11";
+    nix-bundle-exe = {
+      url = "github:3noch/nix-bundle-exe";
       flake = false;
     };
+    gomod2nix.url = "github:tweag/gomod2nix";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, nix-bundle-exe, gomod2nix, flake-utils }:
+    let
+      rev = self.rev or "dirty";
+    in
     (flake-utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = import nixpkgs {
             inherit system;
+            overlays = [
+              self.overlay
+            ];
+            config = { };
           };
         in
         rec {
-          packages = {
-            cronosd = pkgs.callPackage ./. { };
-            cronosd-testnet = pkgs.callPackage ./. { network = "testnet"; };
+          packages = rec {
+            cronosd = pkgs.cronosd;
+            cronosd-testnet = pkgs.cronosd-testnet;
+            cronosd-exe = pkgs.cronosd-exe;
+            cronosd-testnet-exe = pkgs.cronosd-testnet-exe;
           };
           apps = {
             cronosd = {
               type = "app";
-              program = "${packages.cronosd}/bin/cronosd";
+              program = "${pkgs.cronosd}/bin/cronosd";
             };
             cronosd-testnet = {
               type = "app";
-              program = "${packages.cronosd-testnet}/bin/cronosd";
+              program = "${pkgs.cronosd-testnet}/bin/cronosd";
             };
           };
           defaultPackage = packages.cronosd;
@@ -44,5 +55,16 @@
           devShell = devShells.cronosd;
         }
       )
-    );
+    ) // {
+      overlay = final: prev: {
+        buildGoApplication = final.callPackage (import (gomod2nix + "/builder")) {
+          go = final.go_1_17;
+        };
+        bundle-exe = import nix-bundle-exe { pkgs = final; };
+        cronosd = final.callPackage ./. { inherit rev; };
+        cronosd-testnet = final.callPackage ./. { inherit rev; network = "testnet"; };
+        cronosd-exe = final.bundle-exe final.cronosd;
+        cronosd-testnet-exe = final.bundle-exe final.cronosd-testnet;
+      };
+    };
 }
