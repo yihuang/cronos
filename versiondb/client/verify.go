e03e97ef5e3732d75e926d070e71c3ca22266341
HuangYi <huang@crypto.com>
2023-01-06 12:00:22 +0800

    verify multiple continuouse block files

--- versiondb/client/verify.go
+++ versiondb/client/verify.go
@@ -11,48 +11,49 @@ import (
 
 func VerifyPlainFileCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "verify-plain [plain-file]",
-		Short: "Verify change set file by rebuild iavl tree in memory and check root hash",
-		Args:  cobra.ExactArgs(1),
+		Use:   "verify-plain [plain-file] [plain-file] ...",
+		Short: "Verify change set file by rebuild iavl tree in memory and check root hash, the plain files must include continuous blocks",
+		Args:  cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return withPlainInput(args[0], func(reader io.Reader) error {
-				var (
-					err           error
-					latestVersion int64
-				)
-				tree := NewEmptyTree(0)
-				offset, err := readPlainFile(reader, func(version int64, changeSet *versiondb.ChangeSet) error {
-					for _, pair := range changeSet.Pairs {
-						if pair.Delete {
-							tree.Remove(pair.Key)
-						} else {
-							tree.Set(pair.Key, pair.Value)
+			var latestVersion int64
+			tree := NewEmptyTree(0)
+			for _, fileName := range args {
+				if err := withPlainInput(fileName, func(reader io.Reader) error {
+					var err error
+					offset, err := readPlainFile(reader, func(version int64, changeSet *versiondb.ChangeSet) error {
+						for _, pair := range changeSet.Pairs {
+							if pair.Delete {
+								tree.Remove(pair.Key)
+							} else {
+								tree.Set(pair.Key, pair.Value)
+							}
 						}
+
+						// no need to update hashes for intermidiate versions.
+						_, latestVersion, err = tree.SaveVersion(false)
+						if err != nil {
+							return err
+						}
+						return nil
+					}, true)
+
+					if err == io.ErrUnexpectedEOF {
+						// incomplete end of file, we'll output a warning and process the completed versions.
+						fmt.Fprintf(os.Stderr, "file incomplete, the completed versions are processed, the last completed file offset: %d\n", offset)
 					}
 
-					// no need to update hashes for intermidiate versions.
-					_, latestVersion, err = tree.SaveVersion(false)
-					if err != nil {
-						return err
-					}
-					return nil
-				}, true)
-
-				if err == io.ErrUnexpectedEOF {
-					// incomplete end of file, we'll output a warning and process the completed versions.
-					fmt.Fprintf(os.Stderr, "file incomplete, the completed versions are processed, the last completed file offset: %d\n", offset)
-				} else if err != nil {
+					return err
+				}); err != nil {
 					return err
 				}
+			}
 
-				rootHash, err := tree.RootHash()
-				if err != nil {
-					return err
-				}
-				fmt.Printf("%d %X\n", latestVersion, rootHash)
-
-				return nil
-			})
+			rootHash, err := tree.RootHash()
+			if err != nil {
+				return err
+			}
+			fmt.Printf("%d %X\n", latestVersion, rootHash)
+			return nil
 		},
 	}
 	return cmd
